181b3d8068f8366d5749e37fef90bcd06d48ca7c
zulhamsy <zulham.sy@gmail.com>
2023-03-29 11:11:23 +0700

    fixing mutation onAdding Issue

--- src/pages/AddIssue.jsx
+++ src/pages/AddIssue.jsx
@@ -1,41 +1,50 @@
-import { useRef } from "react";
 import { useMutation, useQueryClient } from "react-query";
+import { useNavigate } from "react-router";
 
 export default function AddIssue() {
   const queryClient = useQueryClient();
-  const titleRef = useRef(null);
-  const contentRef = useRef(null);
+  const navigate = useNavigate();
   const issueMutation = useMutation(
-    () =>
+    (payload) =>
       fetch("api/issues", {
         method: "POST",
         headers: {
           "content-type": "application/json",
         },
-        body: JSON.stringify({
-          title: titleRef.current.value,
-          content: contentRef.current.value,
-        }),
-      }),
+        body: JSON.stringify(payload),
+      }).then((res) => res.json()),
     {
-      onSuccess: () => {
+      onSuccess: (data) => {
+        // invalidate query issues agar pas nanti di home, issuesnya refetching walau < staleTime
         queryClient.invalidateQueries(["issues"]);
+        // set data dari respon server ke issue terkait
+        queryClient.setQueryData(["issue", data.number.toString()], data);
+        // arahkan ke halaman issue yang baru aja dibuat
+        navigate(`/issue/${data.number}`);
       },
     },
   );
   function handleSubmit(e) {
     e.preventDefault();
-    issueMutation.mutate();
+    // kalo lagi submitting biar ga dobel
+    if (issueMutation.isLoading) return;
+    issueMutation.mutate({
+      // dapet dari name props yang ada di form
+      title: e.target.title.value,
+      comment: e.target.comment.value,
+    });
   }
   return (
     <div className="add-issue">
       <h2>Add Issue</h2>
       <form onSubmit={handleSubmit}>
         <label htmlFor="title">Title</label>
-        <input ref={titleRef} type="text" id="title" />
-        <label htmlFor="content">Content</label>
-        <textarea ref={contentRef} id="content" />
-        <button type="submit">Add Issue</button>
+        <input type="text" id="title" name="title" />
+        <label htmlFor="comment">Comment</label>
+        <textarea id="comment" name="comment" />
+        <button type="submit" disabled={issueMutation.isLoading}>
+          {issueMutation.isLoading ? "Adding..." : "Add Issue"}
+        </button>
       </form>
     </div>
   );
